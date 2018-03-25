54998a955e1feca402ebf359090f9d47fd274632
Abe Petrillo <abe.petrillo@gmail.com>
2018-03-25 11:28:14 -0400

    authentication done

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,92 +1,38 @@
 /* Contact Form validations start here  */
 (function ($) {
 	'use strict';
-$( "#frm" ).submit(function( event ) {
-	
-  	event.preventDefault();
-  	
-  	var tempn = 0;
-    var tempe = 0;
-	var tempc = 0;
-	var tempa = 0;
-	var tempg = 0;
+	var API_HOST = 'http://localhost:5000'
 
-    var a =$("#Name").val();
-    if(a==""){
-			tempn=1;
+	//TODO: Change this to cookie storage if we have time https://stormpath.com/blog/where-to-store-your-jwts-cookies-vs-html5-web-storage#so-whats-the-difference
+	function storeToken(token) {
+	  window.sessionStorage.accessToken = token;
 	}
-	else{
-			tempn=0;
-	}
-	
-	var a =$("#Emailaddress").val();
-	var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-    if(a==""){
-        tempe=1;
-	}
-	else if (!filter.test(a)) {
-	    tempe=1;
-    }
-	else{
-		tempe=0;
-	}
-	
-	var a =$("#Contactnumber").val();
-    if(a==""){
-        tempc=1;
-	}
-	else if (isNaN(a)) 
-    {
-	    tempc=1;
-    }
-	else{
-		tempc=0;
-	}
-	
-	var a =$("#attendiesDetails").val();
-    if(a==""){
-	    tempa=1;
-	}
-	else{
-		tempa=0;
-	}
-	
-	var a =$("#guestDetails").val();
-    if(a==""){
-			tempg=1;
-	}
-	else{
-			tempg=0;
-	}  	
-  	
-  	if(tempn == 0 && tempe == 0 && tempc == 0 && tempa == 0 && tempg == 0 ){
-  	    $("#loding").css("display","inline-block");
-  	    $.post( "mail_script.php", { 
-		    Name:             $("#Name").val(),
-			Emailaddress:     $("#Emailaddress").val(),
-			Contactnumber:    $("#Contactnumber").val(),
-			attendiesDetails: $("#attendiesDetails").val(),
-			guestDetails:     $("#guestDetails").val(),
-			message:          $("#message").val()
-		})
-        .done(function( data ) {
-  		    if(data==1)	{
-  			    $( "#loding").hide();
-  			    $( "#Success").slideDown( "slow" );
-        		setTimeout(function() {
-                	$( "#Success").slideUp( "slow" );
-            	}, 3000);
-            	$("#frm")[0].reset();
-			}
-			else{
+
+	$( "#frm" ).submit(function( event ) {
+	  	event.preventDefault();
+
+	    $("#loding").css("display","inline-block");
+	    $.post( API_HOST + "/sessions", {
+				invitation: {
+					name: $('#frm input[name=Name]').val(),
+					code: $('#frm input[name=RsvpCode]').val()
+				}
+			}).fail(function(data){
+				$( "#loding").hide();
+		 		$( "#Error").slideDown( "slow" );
+				 setTimeout(function() {
+							 $( "#Error").slideUp( "slow" );
+					 }, 3000);
+			}).done(function( data ) {
+					storeToken(data['auth_token'])
 			    $( "#loding").hide();
-				$( "#Error").slideDown( "slow" );
-        		setTimeout(function() {
-                	$( "#Error").slideUp( "slow" );
-            	}, 3000);
-			}
+			    $( "#Success").slideDown( "slow" );
+
+	    		setTimeout(function() {
+	            	$( "#Success").slideUp( "slow" );
+	        	}, 3000);
+	        $("#frm")[0].reset();
 		});
-  	}	
-});
+	});
 })(jQuery);
 /* Contact Form validation ends here  */
