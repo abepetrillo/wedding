eac4f6bba6eada839290850201005ee8ae29e33c
Abe Petrillo <abe.petrillo@gmail.com>
2018-03-27 15:19:04 -0400

    allow all the things

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'localhost:3000', '127.0.0.1:3000', 'api.abeandsusan.com', '*.abeandsusan.com'
+     origins '*'
 
      resource '*',
        headers: :any,
